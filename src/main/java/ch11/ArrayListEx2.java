f7f66babc8812e26fef7545ab54b56035d9638f6
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-08-20 22:33:29 +0900

    third

--- src/main/java/ch11/ArrayListEx2.java
+++ src/main/java/ch11/ArrayListEx2.java
@@ -11,10 +11,10 @@ public class ArrayListEx2 {
 		String source = "0123456789abcdefghijABCDEFGHIJ!@#$%^&*()ZZZ";
 		int length = source.length();
 
-		List<String> list = new ArrayList<String>(length / LIMIT + 10);
+		List<String> list = new ArrayList<String>(length / LIMIT + 10); //자르고자하는 글자의 개수를 지정
 		for (int i = 0; i < length; i += LIMIT) {
 			if (i + LIMIT < length) {
-				list.add(source.substring(i, i + LIMIT));
+				list.add(source.substring(i, i + LIMIT)); //크기를 약간 여유있게 잡는다.
 			} else {
 				list.add(source.substring(i));
 			}
---
+++ src/main/java/ch11/VectorEx1.java
@@ -0,0 +1,35 @@
+package ch11;
+
+import java.util.Vector;
+
+public class VectorEx1 {
+	public static void print(Vector v) {
+		System.out.println(v);
+		System.out.println("size : " + v.size());
+		System.out.println("capacity : " + v.capacity());
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Vector v = new Vector(5);
+		v.add("1");
+		v.add("2");
+		v.add("3");
+		print(v);
+
+		v.trimToSize(); // 빈공간들을 없앤다
+		System.out.println("===After trimToSize===");
+		print(v);
+
+		v.setSize(7);
+		System.out.println("===After ensureCapacity(6)===");
+		print(v);
+
+		v.clear();
+		System.out.println("After clear()===");
+		print(v);
+
+	}
+
+}
