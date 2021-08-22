0068c9a1fb50cd686e23ba14c63425bfb4c58151
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-08-22 22:34:06 +0900

    이터레이터 실습

---
+++ src/main/java/ch11/MyVector2.java
@@ -0,0 +1,58 @@
+package ch11;
+
+import java.util.Iterator;
+
+public class MyVector2 extends MyVector implements Iterator {
+	int cursor = 0;
+	int lastRet = -1;
+
+	public MyVector2(int capacity) {
+		super(capacity);
+	}
+
+	public MyVector2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String toString() {
+		String tmp = "";
+		Iterator it = iterator();
+		for (int i = 0; it.hasNext(); i++) {
+			if (i != 0) {
+				tmp += ", ";
+			}
+			tmp += it.next();
+		}
+		return "[" + tmp + "]";
+	}
+
+	public Iterator iterator() {
+		cursor = 0;
+		lastRet = -1;
+		return this;
+	}
+
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return cursor != size();
+	}
+
+	@Override
+	public Object next() {
+		// TODO Auto-generated method stub
+		Object next = get(cursor);
+		lastRet = cursor++;
+		return next;
+	}
+
+	public void remove() {
+		if (lastRet == -1) {
+			throw new IllegalStateException();
+		} else {
+			remove(lastRet);
+			cursor--;
+			lastRet = -1;
+		}
+	}
+}
---
+++ src/main/java/ch11/MyVector2Test.java
@@ -0,0 +1,26 @@
+package ch11;
+
+import java.util.Iterator;
+
+public class MyVector2Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyVector2 v = new MyVector2();
+		v.add("0");
+		v.add("1");
+		v.add("2");
+		v.add("3");
+		v.add("4");
+
+		System.out.println("삭제전  : " + v);
+		Iterator it = v.iterator();
+		it.next();
+		it.remove();
+		it.next();
+		it.remove();
+
+		System.out.println("삭제후 : " + v);
+	}
+
+}
