ec34f32843814581492ef43e4772bf9163a8016c
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-09-13 20:29:51 +0900

    지네릭 형변환을 왜하지는지 그리고 타입변수를 해야하는 이유에 대해 공부

---
+++ src/main/java/ch12/Generic/FruitBoxEx2.java
@@ -0,0 +1,76 @@
+package ch12.Generic;
+
+import java.util.ArrayList;
+
+class Fruit implements Eatable {
+	public String toString() {
+		return "Fruit";
+	}
+}
+
+class Apple extends Fruit {
+	public String toString() {
+		return "Apple";
+	}
+}
+
+class Grape extends Fruit {
+	public String toString() {
+		return "Grape";
+	}
+}
+
+class Toy {
+	public String toString() {
+		return "Toy";
+	}
+}
+
+interface Eatable {
+}
+
+public class FruitBoxEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FruitBox<Fruit> fruitBox = new FruitBox<Fruit>();
+		FruitBox<Apple> appleBox = new FruitBox<Apple>();
+		FruitBox<Grape> grapeBox = new FruitBox<Grape>();
+//		FruitBox<Grape> fruitBox<Apple>();
+//		FruitBox<Toy> toyBox = new FruitBox<Toy>();
+
+		fruitBox.add(new Fruit());
+		fruitBox.add(new Apple());
+		fruitBox.add(new Grape());
+		appleBox.add(new Apple());
+//		fruitBox.add(new Grape());
+		grapeBox.add(new Grape());
+
+		System.out.println("fruitBox-" + fruitBox);
+		System.out.println("appleBox-" + appleBox);
+		System.out.println("grapeBox-" + grapeBox);
+	}
+}
+
+class FruitBox<T extends Fruit & Eatable> extends Box<T> {
+}
+
+class Box<T> {
+	ArrayList<T> list = new ArrayList<T>();
+
+	void add(T item) {
+		list.add(item);
+	}
+
+	T get(int i) {
+		return list.get(i);
+	}
+
+	int size() {
+		return list.size();
+	}
+
+	public String toString() {
+		return list.toString();
+	}
+}
