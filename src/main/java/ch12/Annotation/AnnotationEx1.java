49325483b39f8843c357d7f8fcbdfaf1035e5374
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-10-05 19:45:26 +0900

    어노테이션 실습

---
+++ src/main/java/ch12/Annotation/AnnotationEx1.java
@@ -0,0 +1,19 @@
+package ch12.Annotation;
+
+public class AnnotationEx1 {
+
+}
+
+class Parent {
+	void parentMethod() {
+
+	}
+}
+
+class Child extends Parent {
+
+	@Override
+	void parentMethod() {
+
+	}
+}
