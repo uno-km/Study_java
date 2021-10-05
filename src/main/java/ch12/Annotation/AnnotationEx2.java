7ce21adff41613c4aeeae531ce00c89b03107ffd
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-10-05 20:12:39 +0900

    펑셔널애너테이션 실습

--- src/main/java/ch12/Annotation/AnnotationEx2.java
+++ src/main/java/ch12/Annotation/AnnotationEx2.java
@@ -27,3 +27,12 @@ class NewClass extends Class {
 		return oldField;
 	}
 }
+
+@FunctionalInterface
+interface Testable { // 함수형 인터페이스는 하나의 추상메서드만 가능하거든
+	void test(); // 추상메서드
+
+//	void check(); //추상메서드2 
+	// 그래서 추상메서드2를 주석처리하니 에러가 안생김
+	// 내가 잘못 입력해서 생기는 오류를 잡아준다.
+}
