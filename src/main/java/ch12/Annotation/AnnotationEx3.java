d545679cd9bf578732937bbc8185d533223b54c6
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-10-05 20:23:28 +0900

    경고처리 실습2

---
+++ src/main/java/ch12/Annotation/AnnotationEx3.java
@@ -0,0 +1,31 @@
+package ch12.Annotation;
+
+import java.util.ArrayList;
+
+public class AnnotationEx3 {
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void main(String args[]) {
+		nclass nc = new nclass();
+
+		nc.oldField = 10;
+		System.out.println(nc.getnewField());
+
+		ArrayList<nclass> list = new ArrayList();
+		list.add(nc);
+	}
+}
+
+class nclass {
+	int newField;
+	@Deprecated
+	int oldField;
+
+	int getnewField() {
+		return newField;
+	}
+
+	@Deprecated
+	int getOldField() {
+		return oldField;
+	}
+}
