96cc69784fe1ba90e6955e6105d6a787eccf6ca3
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-10-08 23:24:47 +0900

    멀티쓰레드를 실험했다. 쓰레드 강제종료 먹힌다.

--- src/main/java/ch13/Thread/ThreadEx6.java
+++ src/main/java/ch13/Thread/ThreadEx6.java
@@ -7,7 +7,7 @@ public class ThreadEx6 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String input = JOptionPane.showInputDialog("아무값이나 입력하세요.");
-		System.out.println("your entered is " + input);
+		System.out.println("당신이 입력한 값 : " + input);
 		for (int i = 10; i > 0; i--) {
 			System.out.println(i);
 			try {
