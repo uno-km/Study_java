7e1efac22e7de29f851bc35d931b7b00665da35a
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-10-08 22:55:04 +0900

    싱글쓰레드와 멀티쓰레드의 계산차이와 그 이유를 공부 및 실습

--- src/main/java/ch13/Thread/ThreadEx4.java
+++ src/main/java/ch13/Thread/ThreadEx4.java
@@ -4,12 +4,12 @@ public class ThreadEx4 {
 
 	public static void main(String[] args) {
 		long startTime = System.currentTimeMillis();
-		for (int i = 0; i < 300; i++) {
+		for (int i = 0; i < 10000; i++) {
 			System.out.printf("%s", new String("-"));
 
 		}
 		System.out.println("소요시간 : " + (System.currentTimeMillis() - startTime));
-		for (int i = 0; i < 300; i++) {
+		for (int i = 0; i < 10000; i++) {
 			System.out.printf("%s", new String("|"));
 		}
 		System.out.println("소요시간2 : " + (System.currentTimeMillis() - startTime));
