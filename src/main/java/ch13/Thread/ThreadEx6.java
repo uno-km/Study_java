536fba138cd1f248a64cc77274f738f4cfb39b67
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-10-08 23:01:02 +0900

    싱글코어의 작업, 입출력 작업

---
+++ src/main/java/ch13/Thread/ThreadEx6.java
@@ -0,0 +1,22 @@
+package ch13.Thread;
+
+import javax.swing.JOptionPane;
+
+public class ThreadEx6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String input = JOptionPane.showInputDialog("아무값이나 입력하세요.");
+		System.out.println("your entered is " + input);
+		for (int i = 10; i > 0; i--) {
+			System.out.println(i);
+			try {
+				Thread.sleep(1000);
+
+			} catch (Exception e) {
+
+			}
+		}
+	}
+
+}
