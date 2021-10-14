725f1a48f73b8b4ee5fc027499908a81f2e941c2
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-10-14 21:04:54 +0900

    조인메서드를 통해 메인쓰레드에서 시간을 재고, 또 그것을 주석처리하므로써 차이점 실습

---
+++ src/main/java/ch13/Thread/ThreadEx20.java
@@ -0,0 +1,56 @@
+package ch13.Thread;
+
+public class ThreadEx20 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ThreadEx_20 gc = new ThreadEx_20();
+		gc.setDaemon(true);
+		gc.start();
+
+		int requireMemory = 0;
+		for (int i = 0; i > 20; i++) {
+			requireMemory = (int) (Math.random() * 10) * 20;
+			if (gc.freeMemory() < requireMemory || gc.freeMemory() < gc.totalMemory() * 0.4) {
+				gc.interrupt();
+			}
+			gc.usedMemory += requireMemory;
+			System.out.println("usedMemory : " + gc.usedMemory);
+		}
+	}
+
+}
+
+class ThreadEx_20 extends Thread {
+	final static int MAX_MEMORY = 1000;
+	int usedMemory = 0;
+
+	public void run() {
+		while (true) {
+			try {
+				Thread.sleep(10 * 1000);
+
+			} catch (InterruptedException e) {
+				System.out.println("Awaken by Free Memory : " + freeMemory());
+
+			}
+			gc();
+			System.out.println("Garbage Collected, Free Memory : " + freeMemory());
+		}
+	}
+
+	public void gc() {
+		usedMemory -= 300;
+		if (usedMemory < 0) {
+			usedMemory = 0;
+		}
+	}
+
+	public int totalMemory() {
+		return MAX_MEMORY;
+	}
+
+	public int freeMemory() {
+		return MAX_MEMORY - usedMemory;
+	}
+}
