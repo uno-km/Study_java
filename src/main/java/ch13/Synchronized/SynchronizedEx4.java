23aa4fce811d6e76ae21fee32ac0f027bb0bc1de
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-10-15 19:15:07 +0900

    wait()과 notify()를 추가하여 자연스럽게 처리하도록 했다.

--- src/main/java/ch13/Synchronized/SynchronizedEx4.java
+++ src/main/java/ch13/Synchronized/SynchronizedEx4.java
@@ -3,54 +3,38 @@ package ch13.Synchronized;
 import java.util.ArrayList;
 
 class Customer2 implements Runnable {
-	private Table2 table;
+	private Table2 table2;
 	private String food;
-
-	Customer2(Table2 table, String food) {
-		this.table = table;
+	Customer2(Table2 table2, String food) {
+		this.table2 = table2;
 		this.food = food;
 	}
-
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		while (true) {
-			try {
-				Thread.sleep(10);
-			} catch (InterruptedException e) {
-			}
+			try {Thread.sleep(10);} catch (InterruptedException e) {}
 			String name = Thread.currentThread().getName();
-			if (eatFood()) {
-				System.out.println(name + " ate a " + food);
-			} else {
-				System.out.println(name + " failed to eat... : (");
-			}
+			
+			table2.remove(food);
+			System.out.println(name + " ate a" + food);
 		}
-	}
-
-	boolean eatFood() {
-		return table.remove(food);
 	}
 }
 
-class Cook2 implements Runnable {
-	private Table2 table;
 
-	Cook2(Table2 table) {
-		this.table = table;
-	}
+class Cook2 implements Runnable {
+	private Table2 table2;
+
+	Cook2(Table2 table2) {this.table2 = table2;}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		while (true) {
-			int idx = (int) (Math.random() * table.dishNum());
-			table.add(table.dishNames[idx]);
-			try {
-				Thread.sleep(1);
-			} catch (InterruptedException e) {
-
-			}
+			int idx = (int) (Math.random() * table2.dishNum());
+			table2.add(table2.dishNames[idx]);
+			try {Thread.sleep(10);} catch (InterruptedException e) {}
 		}
 	}
 }
@@ -61,49 +45,58 @@ class Table2 {
 	private ArrayList<String> dishes = new ArrayList<>();
 
 	public synchronized void add(String dish) {
-		if (dishes.size() >= MAX_FOOD) {
-			return;
+		while (dishes.size() >= MAX_FOOD) {
+			String name = Thread.currentThread().getName();
+			System.out.println(name + " is waiting. <-테이블의 음식이 소비되지 않아서 ");
+			try {
+				wait();
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+
+			}
 		}
 		dishes.add(dish);
+		notify();
 		System.out.println("Dished : " + dishes.toString());
 	}
 
 	public boolean remove(String dishName) {
 		synchronized (this) {
+			String name = Thread.currentThread().getName();
 			while (dishes.size() == 0) {
-				String name = Thread.currentThread().getName();
-				System.out.println(name + " is waiting.");
+				System.out.println(name + " is waiting. <- 원하는 음식이 없어서 기다리고 있다.");
 				try {
+					wait();
 					Thread.sleep(500);
-				} catch (InterruptedException e) {
-
-				}
+				} catch (InterruptedException e) {}
 			}
-			for (int i = 0; i < dishes.size(); i++)
-				if (dishName.equals(dishes.get(i))) {
-					dishes.remove(i);
-					return true;
+			while(true) {
+				for (int i = 0; i < dishes.size(); i++) {
+					if (dishName.equals(dishes.get(i))) {
+						dishes.remove(i);
+						notify();
+						return true;
+					}
 				}
+				try {
+					System.out.println(name + " is waiting.");
+					wait();
+					Thread.sleep(500);
+				}catch(InterruptedException e) {}
+			}
 		}
-		return false;
 	}
-
-	public int dishNum() {
-		return dishNames.length;
-	}
+	public int dishNum() {return dishNames.length;}
 }
 
 public class SynchronizedEx4 {
-
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		Table2 table = new Table2();
+		Table2 table2 = new Table2();
 
-		new Thread(new Cook2(table), "COOK1").start();
-		new Thread(new Customer2(table, "pizza"), "CUST1").start();
-		new Thread(new Customer2(table, "bread"), "CUST2").start();
-		Thread.sleep(5000);
-		System.exit(0);
+		new Thread(new Cook2(table2), "COOK2").start();
+		new Thread(new Customer2(table2, "pizza"), "CUST1").start();
+		new Thread(new Customer2(table2, "bread"), "CUST2").start();
 	}
 
 }
