036c079e2c68f01f536d8d34e1150d1136f28b9b
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-11-02 23:54:15 +0900

    람다식의 타입을 어떤 형식으로 만드는지 실습

---
+++ src/main/java/ch14/Lambda/LambdaEx2.java
@@ -0,0 +1,20 @@
+package ch14.Lambda;
+
+interface MyFunction2 {
+	void myMethod();
+}
+
+public class LambdaEx2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyFunction2 f = () -> {};
+		Object o = (MyFunction2) (() -> {});
+		String str = ((Object)(MyFunction)(()->{})).toString();
+		Object c = ((MyFunction)(()->{})).getClass();
+		System.out.println("함수인터페이스의 값 : "+f);
+		System.out.println("오브젝트형변환 값 : " + o);
+		System.out.println("문자열 형변환 값" + str);
+		System.out.println("클래스 : " +c);
+		System.out.println((MyFunction2)(()->{}));
+	}
+}
