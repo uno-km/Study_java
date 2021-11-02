d2fe07a49d731b172d09f4ba9f2b0492328a6e59
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-11-02 22:42:35 +0900

    자료구조 공부 및 람다식 공부

--- src/main/java/ch11/HashMap/Test.java
+++ src/main/java/ch11/HashMap/Test.java
@@ -1,13 +1,29 @@
 package ch11.HashMap;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		HashMap map = new HashMap();
+		ArrayList list = new ArrayList();
+		list.add(map);
+		list.add("김건호");
+		list.add("김찬호");
+		list.add("김찬호");
+		map.put("가족",list);
 
+		Set set = map.entrySet();
+		Iterator it = set.iterator();
+		while (it.hasNext()) {
+			Map.Entry e = (Map.Entry) it.next();
+			System.out.println(e.getKey() + " : " + e.getValue());
+		}
 	}
 
 }
