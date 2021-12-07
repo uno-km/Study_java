77272abedd5bdc5a502f8b94cb2db26bae3cf1a3
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-12-07 23:28:38 +0900

    외부 변수 참조

--- src/main/java/ch14/Lambda/LambdaEx3.java
+++ src/main/java/ch14/Lambda/LambdaEx3.java
@@ -4,6 +4,29 @@ package ch14.Lambda;
 interface ex3fn1 {
 	public abstract void ex3fn1();
 }
+@FunctionalInterface
+interface myFunction_ex3 {
+	public void myFunction_ex3() ;
+}
+class Outer{
+	int val =10;
+	class Inner{
+		int val =20;
+		void method(int i) {
+			int val = 30;
+			myFunction_ex3 fn1= ()->{
+				System.out.println("                i : "+i);
+				System.out.println("              val : " + val);
+				System.out.println("         this.val : " + this.val);
+				System.out.println("   Outer.this.val : " + Outer.this.val);
+			};
+			fn1.myFunction_ex3();
+			
+		}
+	}
+	
+	
+}
 
 public class LambdaEx3 {
 	public static void main(String[] args) {
@@ -16,5 +39,10 @@ public class LambdaEx3 {
 
 		System.out.println((ex3fn1)(()->{}));
 		System.out.println(((Object)(ex3fn1)(()->{})).toString());
+		System.out.println("-----------------------");
+		Outer out = new Outer();
+		Outer.Inner in = out.new Inner();
+		in.method(100);
+		
 	}
 }
