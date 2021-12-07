985bc6ae13944da2254d9e31a06d43ae05aef1f0
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-12-07 22:25:42 +0900

    람다식 공부

--- src/main/java/ch14/Lambda/LambdaEx0.java
+++ src/main/java/ch14/Lambda/LambdaEx0.java
@@ -5,10 +5,17 @@ interface MyFunction0 {
 	public abstract int max(int a, int b);
 }
 
+@FunctionalInterface
+interface MyFunction1 {
+	public abstract int min(int a, int b);
+}
+
 public class LambdaEx0 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MyFunction0 f = (a, b) -> a > b ? a : b;
+		MyFunction1 f2 = (a, b) -> a < b ? a : b;
 		System.out.println(f.max(3, 5));
+		System.out.println(f2.min(3, 5));
 	}
 }
